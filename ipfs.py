f92c290bb523fab14e61ecef0da3244538736b92
Alvin Eng <alvineng@seas.upenn.edu>
2026-03-02 00:50:09 +0000

    Hardcode Pinata JWT for grading

--- ipfs.py
+++ ipfs.py
@@ -1,17 +1,17 @@
-import os
 import json
 import requests
+
+PINATA_JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIxN2YwNWZhZS0wNmIzLTRkOTQtYWM1MS1jZDFlODFiZjE3ZjEiLCJlbWFpbCI6ImVuZ2hhbndlbmFsdmluQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJmMTdhNzU1M2UyZTNmNGYzOTJhMiIsInNjb3BlZEtleVNlY3JldCI6IjdhMDVkNmU0M2ZlMGU5NGUzNjI1OTIzNTI0YjUyZjFjYzk5MDExNDRlZWQyMTY1ZmE5MGVkNWVlYzE0NWVjMTYiLCJleHAiOjE4MDM5MDU2MzR9.fHHhzH29_ZOuKzBrzQCUrHoysyRyPL8hNm7UdJsXgus"
 
 PINATA_PIN_JSON_URL = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
 
 def pin_to_ipfs(data):
     assert isinstance(data, dict), "Error pin_to_ipfs expects a dictionary"
 
-    jwt = os.getenv("PINATA_JWT") or os.getenv("PINATA_JWT")
-    if not jwt:
-        raise RuntimeError("PINATA_JWT environment variable not set")
-
-    headers = {"Authorization": f"Bearer {jwt}"}
+    headers = {
+        "Authorization": f"Bearer {PINATA_JWT}",
+        "Content-Type": "application/json",
+    }
 
     r = requests.post(PINATA_PIN_JSON_URL, headers=headers, json=data, timeout=30)
     r.raise_for_status()
